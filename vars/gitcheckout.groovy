81c0efbcfb77523363d206914fa2d358d8d765c9
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2020-12-28 00:09:29 +0700

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -7,7 +7,8 @@ def call(String mygiturl = 'https://github.com/trungducnguyen92/Test_repo.git'){
 	    echo "Your repo url is: ${mygiturl}"
 	    echo "Now cloning the repository"
 	    sh 'git clone https://github.com/trungducnguyen92/Test_repo.git'
-	    sh 'ls -a'
+	    sh 'cd /var/lib/jenkins/workspace/exercise-git-checkout/Test_repo/'
+	    sh 'ls -l'
 	    /*sh 'git checkout test1'*/
 	    /*sh 'git branch'*/
         }
