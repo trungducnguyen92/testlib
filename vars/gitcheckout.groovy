7ca3cc2cf59efae7683dd2512a737680ca980b0d
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2020-12-27 23:49:57 +0700

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -2,9 +2,12 @@ def call(String mygiturl = 'https://github.com/trungducnguyen92/Test_repo.git'){
     pipeline {
       agent any
       stages {
-        stage('Even Stage') {
+        stage('Checkout git') {
           steps {
 	    echo "Your repo url is: ${mygiturl}"
+	    echo "Now cloning the repository"
+	    sh 'git clone ${mygiturl}'
+	    sh 'cd /Test_repo'
 	    sh 'ls -a'
         }
       }
