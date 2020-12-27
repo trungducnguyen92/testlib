2161f46b2974d2cc2b82166201e5812c9b1d1f5e
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2020-12-27 23:53:16 +0700

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -6,7 +6,7 @@ def call(String mygiturl = 'https://github.com/trungducnguyen92/Test_repo.git'){
           steps {
 	    echo "Your repo url is: ${mygiturl}"
 	    echo "Now cloning the repository"
-	    sh 'git clone ${mygiturl}'
+	    sh 'git clone https://github.com/trungducnguyen92/Test_repo.git'
 	    sh 'cd /Test_repo'
 	    sh 'ls -a'
         }
