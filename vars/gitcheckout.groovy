52b3e361e0c82e99f9de8cee9e0c229434ef08e7
trungducnguyen92 <65993183+trungducnguyen92@users.noreply.github.com>
2020-12-27 21:35:59 +0700

    Update gitcheckout.groovy

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String mygiturl = 'https://github.com/trungducnguyen92/Test_repo.git'){
-	echo "Your repo url is: ${mygiturl}
+	echo "Your repo url is: ${mygiturl}"
 }
